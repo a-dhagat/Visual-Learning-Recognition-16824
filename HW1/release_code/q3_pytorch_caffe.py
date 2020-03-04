24eec991e2657207001e271e124d45ace9903195
a-dhagat <adhagat@andrew.cmu.edu>
2020-03-04 13:23:34 -0500

    Added q3 with in-development _init_ for CaffeNet

---
+++ HW1/release_code/q3_pytorch_caffe.py
@@ -0,0 +1,40 @@
+from __future__ import print_function
+
+import numpy as np
+import torch
+
+import utils
+# from q0_hello_mnist import SimpleCNN
+from voc_dataset import VOCDataset
+
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+class CaffeNet(nn.Module):
+    def __init__(self, num_classes=20, inp_size=256, c_dim=3):
+        '''
+        conv(kernel_size, stride, out_channels, padding)
+        nn.conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        VALID: uses only valid input data if stride such that not all data can be used --> No padding
+        SAME: keeps the output size same as the input size ------------------------------> Applies necessary padding to do that
+
+        Conv Layer Accepts a volume of size W1×H1×D1
+            Requires four hyperparameters:
+                Number of filters K, their spatial extent F, the stride S, the amount of zero padding P.
+            Produces a volume of size W2×H2×D2 where:
+                W2=(W1−F+2P)/S+1
+                H2=(H1−F+2P)/S+1 (i.e. width and height are computed equally by symmetry)
+                D2=K
+
+        max_pool(kernel_size, stride)
+        nn.MaxPool2d(kernel_size, stride)
+        '''
+        super().__init__()
+        self.num_classes = num_classes
+        self.conv1 = nn.Conv2d(c_dim, 96, 11, 4, padding=0) # conv(11,4,96,'VALID')
+        
+        self.conv2 = nn.Conv2d(c_dim, 96, 11, 4, padding=) # conv(5,1,256,'SAME')
+        self.non_linear = lambda x: F.relu(x, inpalce=True)
+        self.pool1 = nn.MaxPool2d(3,2)
+
